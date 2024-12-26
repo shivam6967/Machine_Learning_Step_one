45fcc700df2ca721eb3bb3064956f5010085082c
shivammathapati <156904099+shivam6967@users.noreply.github.com>
2024-12-27 01:00:37 +0530

    The are seven days in a week

---
+++ pythonOne.py
@@ -0,0 +1,20 @@
+# The First Program in Python  
+
+print("Shivam Vivekanand Mathapti")
+
+
+# to run the code , in terminal 
+# python file_name 
+
+
+# we can install any module (the code written by someone else so that we can use for our work ) by using the p
+# pip install module_name
+
+# ex .  pip install pyjokes 
+
+import pyjokes
+
+joke = pyjokes.get_joke();
+print(joke)
+
+
